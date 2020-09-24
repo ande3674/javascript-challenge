5c927ef1731ecb34f7c3add1737540d55c95ec3d
AliAnderson <alexis.anderson@target.com>
2020-09-23 20:26:44 -0500

    add search functionality

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,75 @@
 // from data.js
 var tableData = data;
 
-// Get a reference to the table body
+// Get a reference to the dom objects
 var tbody = d3.select("tbody");
 var tableObject = d3.select("#ufo-table");
+var filterButton = d3.select("#filter-btn");
 
-// loop through all data
+// loop through all data & add to the table
 tableData.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
-    
     // append a new row to the table
     var newRow = tbody.append("tr");
 
     // add value from each key-value pair to its table cell
     Object.entries(ufoSighting).forEach(function([key, val]) {
-        console.log(key, val);
-
         // append a cell
         var newCell = newRow.append("td");
         // write value to cell
         newCell.text(val);
 
     });
+});
 
+// listen for events and search through the date/time column to find rows that match user input
+// form button: id="filter-btn"
+// input area: id="datetime"
+filterButton.on("click", function() {
+    // Get form text
+    var formInput = d3.select("#datetime");
+    var formInputValue = formInput.property("value");
+  
+    // Filter table on date
+    var filteredData = tableData.filter(sighting => sighting.datetime === formInputValue);
+    console.log(filteredData);
+    // clear the table
+    tbody.html("");
+    // add the filtered data
+    filteredData.forEach(function(ufoSighting) {
+        // append a new row to the table
+        var newRow = tbody.append("tr");
+
+        // add value from each key-value pair to its table cell
+        Object.entries(ufoSighting).forEach(function([key, val]) {
+            console.log(key, val);
+            // append a cell
+            var newCell = newRow.append("td");
+            // write value to cell
+            newCell.text(val);
+
+        });
+    });
+  
 });
 
 
+// function to populate table
+// function populateTable(tableData) {
+//     // append a new row to the table
+//     var newRow = tbody.append("tr");
+
+//     // add value from each key-value pair to its table cell
+//     Object.entries(tableData).forEach(function([key, val]) {
+//         console.log(key, val);
+
+//         // append a cell
+//         var newCell = newRow.append("td");
+//         // write value to cell
+//         newCell.text(val);
+// };
 
 
-// Step 5: Use d3 to update each cell's text with weather report values (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//     console.log(weatherReport);
-//     var row = tbody.append("tr");
-//     Object.entries(weatherReport).forEach(function([key, value]) {
-//       console.log(key, value);
-//       // Append a cell to the row for each value
-//       // in the weather report object
-//       var cell = row.append("td");
-//       cell.text(value);
-//     });
-//   });
+
+
+
+
